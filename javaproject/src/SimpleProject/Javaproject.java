a6259ba7afacebe23db5f45287c7a976018db0d0
user <user@user-PC.domain.name>
2019-02-25 15:40:16 +0530

    First commit

---
+++ javaproject/src/SimpleProject/Javaproject.java
@@ -0,0 +1,14 @@
+package SimpleProject;
+
+public class Javaproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str="javaDeveloper";
+		System.out.println(str.toUpperCase());
+		
+		
+	}
+
+}
